9f7b0345ea0202622a25702fc6fd7a76596a5c3e
markosjal <markosjal@gmail.com>
2019-05-29 20:34:45 -0500

    Update ScannerStatus.php

--- webroot/eSCL/ScannerStatus.php
+++ webroot/eSCL/ScannerStatus.php
@@ -46,7 +46,7 @@ $scannercapabilities='<?xml version="1.0" encoding="UTF-8"?>
                         <scan:MinWidth>300</scan:MinWidth>
                         <scan:MaxWidth>2575</scan:MaxWidth>
                         <scan:MinHeight>300</scan:MinHeight>
-                        <scan:MaxHeight>4784</scan:MaxHeight>
+                        <scan:MaxHeight>4783</scan:MaxHeight>
                         <scan:MaxScanRegions>1</scan:MaxScanRegions>
                         <scan:SettingProfiles>
                                 <scan:SettingProfile>
